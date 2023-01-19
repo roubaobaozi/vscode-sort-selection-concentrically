0a8d6604e1224ade0d042cafd406c04fc9f1fb36
baobaozi <work@mingteo.com>
2023-01-19 14:24:56 +1100

    test more dependency updates

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,3 +1,5 @@
+import * as path from 'path';
+
 //
 // PLEASE DO NOT MODIFY / DELETE UNLESS YOU KNOW WHAT YOU ARE DOING
 //
@@ -9,13 +11,34 @@
 // host can call to run the tests. The test runner is expected to use console.log
 // to report the results back to the caller. When the tests are finished, return
 // a possible error to the callback or null if none.
-import * as testRunner from 'vscode/lib/testrunner';
+// import * as testRunner from 'vscode/lib/testrunner';
+import { runTests } from '@vscode/test-electron';
 
 // You can directly control Mocha options by uncommenting the following lines
 // See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options for more info
-testRunner.configure({
-    ui: 'tdd', 		// the TDD UI is being used in extension.test.ts (suite, test, etc.)
-    useColors: true // colored output from test results
-});
+// runTests.configure({
+//     ui: 'tdd', 		// the TDD UI is being used in extension.test.ts (suite, test, etc.)
+//     useColors: true // colored output from test results
+// });
 
-module.exports = testRunner;
+// module.exports = testRunner;
+
+async function main() {
+    try {
+        // The folder containing the Extension Manifest package.json
+        // Passed to `--extensionDevelopmentPath`
+        const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+
+        // The path to test runner
+        // Passed to --extensionTestsPath
+        const extensionTestsPath = path.resolve(__dirname, './');
+
+        // Download VS Code, unzip it and run the integration test
+        await runTests({ extensionDevelopmentPath, extensionTestsPath });
+    } catch (err) {
+        console.error('Failed to run tests');
+        process.exit(1);
+    }
+}
+
+main();
